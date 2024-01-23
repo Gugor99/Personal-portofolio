8c84dd20e5c69bf7c37b9afbd084ccd741abad85
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2024-01-23 18:31:10 +0100

    Fixed layout, Links and added projects buttons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,7 +31,7 @@ export const Projects = () => {
       title: "Quizzcall",
       description: "React & APIs",
       imgUrl: projImg3,
-      githubUrl: "",
+      githubUrl: "https://github.com/Gugor99/Quizzcall",
       siteUrl: "https://quizzle-react-gaby.netlify.app/",
     },
     {
@@ -66,7 +66,7 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>A selection of my range of work</p>
                 <Row>
                         {
                           projects.map((project, index) => {
