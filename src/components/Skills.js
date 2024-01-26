054ebc971deda9ea1813b132314f46bae2766d93
Gugor99 <121721730+Gugor99@users.noreply.github.com>
2024-01-26 10:53:32 +0100

    Alt tag fixed

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -72,7 +72,7 @@ export const Skills = () => {
                 </div>
             </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+        <img className="background-image-left" src={colorSharp} alt="Blurred red and grey on dark background" />
     </section>
   )
 }
